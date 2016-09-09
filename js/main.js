b1035a4ce12aa72cf00c4127df1c58ae66b7aebe
mel1bt <mberrocal.tipte@gmail.com>
2016-09-09 12:58:23 -0500

    formulario corregido

--- js/main.js
+++ js/main.js
@@ -4,19 +4,21 @@ window.addEventListener("load", function() {
         var nom = document.getElementById("nombre").value;
         var edad = parseInt(document.getElementById("edad").value);
         var genero = document.querySelector('input[name="genero"]:checked').value;
-        var resultado = document.getElementById("resultado");
+        
         var prueba = new Persona(nom, edad, genero);
-        resultado.innerHTML = document.write(prueba.presentacion());
+        
+        var resultado = document.getElementById("resultado");
+        resultado.innerHTML = prueba.presentacion();
        
     });
-    
-    function Persona(nombre,edad,sexo){
+ });   
+function Persona(nombre,edad,sexo){
         this.nombre = nombre;
         this.edad = edad;
         this.sexo = sexo;
         this.mayorDeEdad = (this.edad >= 18) ? true : false;
         this.presentacion = function(){
-        return  "Hola, mi nombre es " + this.nombre + " tengo " + this.edad + " años y soy " + ( (this.mayorDeEdad) ? "mayor" : "menor")  + " de edad. ";      
-        };
+        return "Hola, mi nombre es " + this.nombre + " tengo " + this.edad + " años y soy " + ( (this.mayorDeEdad) ? "mayor" : "menor")  + " de edad. ";      
         }
-});
+ }
+
